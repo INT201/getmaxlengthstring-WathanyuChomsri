e65cd00d138df816b88667a716e1e71818f3d2bd
WathanyuChomsri <wathanyu.chom@kmutt.ac.th>
2022-09-28 21:59:37 +0700

    get

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -40,3 +40,29 @@ function getMaxLengthString(arrayOfString) {
 // console.log(getMaxLengthString(undefined) )
 
 module.exports = getMaxLengthString
+
+ // if (arrayOfString == (null || undefined)) {
+  //   undefined
+
+  // } else {
+
+  //   let arrString = arrayOfString[0].length
+  //   let objequal = []
+
+
+
+  //   for (let i = 0; i < arrayOfString.length; i++) {
+
+  //     if (arrayOfString[i].length > arrString) {
+  //       arrString = arrayOfString[i].length
+  //     }
+  //   }
+
+  //   for (let i = 0; i < arrayOfString.length; i++) {
+  //     if (arrayOfString[i].length == arrString) {
+  //       objequal.push(arrayOfString[i])
+  //     }
+  //   }
+
+  //   return objequal
+  // }
